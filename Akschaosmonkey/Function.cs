852c7275487efdc44c63d7bfeb175acfe47a385a
Daniel Semedo <danpin@microsoft.com>
2019-02-07 15:00:10 -0200

    list all pods in the container and kubernetes client is working

--- Akschaosmonkey/Function.cs
+++ Akschaosmonkey/Function.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using k8s;
 
 namespace Akschaosmonkey
 {
@@ -17,13 +18,32 @@ namespace Akschaosmonkey
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("HTTP trigger function processed a request.");
 
             string command = req.Query["command"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             command = command ?? data?.command;
+
+            if(command == "scale")
+            {
+
+                var config = KubernetesClientConfiguration.BuildConfigFromConfigFile(@"C:\config.txt");
+                IKubernetes client = new Kubernetes(config);
+                log.LogInformation("Starting Request!");
+
+                var list = client.ListNamespacedPod("dev");
+                foreach (var item in list.Items)
+                {
+                    log.LogInformation(item.Metadata.Name);
+                }
+                if (list.Items.Count == 0)
+                {
+                    log.LogInformation("Empty!");
+                }
+
+            }
 
             return command != null
                 ? (ActionResult)new OkObjectResult($"Scaling your AKS instance in 1 replica, command: {command}")
